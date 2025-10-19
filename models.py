837c35280a386e00197604475dbf0482707877d9
Justin <justin022306@gmail.com>
2025-10-19 01:32:44 -0400

    updated some ui

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # <-- singular
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(255), nullable=False)
 
@@ -16,7 +16,7 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, pw)
 
 class Quote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # <-- singular
+    id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(280), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
